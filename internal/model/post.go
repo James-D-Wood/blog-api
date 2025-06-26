c9b62a33f43fd41dc2caef06cd36594c34493401
James Wood <woodjames275@gmail.com>
2025-06-25 18:48:39 -0700

    chore: rename blog to posts

---
+++ internal/model/post.go
@@ -0,0 +1,22 @@
+package model
+
+// statuses
+
+type BlogPostStatus string
+
+const (
+	PUBLISHED BlogPostStatus = "PUBLISHED"
+	DRAFT     BlogPostStatus = "DRAFT"
+)
+
+type BlogPost struct {
+	ID          string         `json:"id"`
+	Status      BlogPostStatus `json:"status"`
+	Title       string         `json:"title"`
+	Summary     string         `json:"summary"`
+	Contents    string         `json:"contents"`
+	AuthorID    string         `json:"author_id"`
+	CreatedTS   string         `json:"created_ts"`
+	PublishedTS string         `json:"published_ts"`
+	UpdatedTS   string         `json:"updated_ts"`
+}
