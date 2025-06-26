8894dc94d2bb071bc1e2a38e18497ec0b9d34442
James Wood <woodjames275@gmail.com>
2025-06-25 22:25:37 -0700

    feat: update and delete handlers for the blog post entity

--- internal/api/posts.go
+++ internal/api/posts.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/James-D-Wood/blog-api/internal/httputils"
@@ -17,6 +18,19 @@ func (app *App) FetchBlogPostHandler(w http.ResponseWriter, r *http.Request) {
 		app.Logger.Error("failed to fetch blog post", "error", err, "location", "FetchBlogPostHandler")
 		httputils.RespondWithJsonError(w, "failed to fetch blog post", 404)
 		return
+	}
+
+	userID, err := httputils.GetUserFromContext(r.Context())
+	if err != nil {
+		app.Logger.Warn("failed to identify user", "error", err, "location", "FetchBlogPostHandler")
+	}
+
+	if post.Status == model.DRAFT {
+		if post.AuthorID != userID {
+			app.Logger.Error("user not authorized to view blog post", "error", err, "location", "FetchBlogPostHandler")
+			httputils.RespondWithJsonError(w, "user not authorized to view this post", 403)
+			return
+		}
 	}
 
 	type Response struct {
@@ -56,7 +70,14 @@ func (app *App) CreateBlogPostHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	err = app.BlogService.CreateBlogPost(r.Context(), &post)
+	userID, err := httputils.GetUserFromContext(r.Context())
+	if err != nil || userID == "" {
+		app.Logger.Error("failed to identify user", "error", err, "location", "CreateBlogPostHandler")
+		httputils.RespondWithJsonError(w, "internal service error", 500)
+		return
+	}
+
+	err = app.BlogService.CreateBlogPost(r.Context(), userID, &post)
 	if err != nil {
 		// TODO: typed errors for better client responses
 		app.Logger.Error("failed to persist blog post", "error", err, "location", "CreateBlogPostHandler")
@@ -74,13 +95,108 @@ func (app *App) CreateBlogPostHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *App) UpdateBlogPostHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("not implemented"))
+	defer r.Body.Close()
+
+	postID := r.PathValue("id")
+
+	var revisedPost model.BlogPost
+	err := json.NewDecoder(r.Body).Decode(&revisedPost)
+	if err != nil {
+		app.Logger.Error("failed to read blog post payload", "error", err, "location", "UpdateBlogPostHandler")
+		httputils.RespondWithJsonError(w, "invalid request body", 400)
+		return
+	}
+
+	storedPost, err := app.BlogService.FetchBlogPost(r.Context(), postID)
+	if err != nil {
+		app.Logger.Error("blog post for given ID does not exist", "error", err, "location", "UpdateBlogPostHandler")
+		httputils.RespondWithJsonError(w, fmt.Sprintf("invalid request: blog post with ID %s does not exist", postID), 400)
+		return
+	}
+
+	// validate user owns resource
+	userID, err := httputils.GetUserFromContext(r.Context())
+	if err != nil {
+		app.Logger.Error("failed to identify user", "error", err, "location", "UpdateBlogPostHandler")
+		httputils.RespondWithJsonError(w, "internal service error", 500)
+		return
+	}
+
+	if storedPost.AuthorID != userID {
+		app.Logger.Error("requestor does not own the blog post they are editing", "location", "UpdateBlogPostHandler", "originalAuthor", storedPost.AuthorID, "requestor", userID)
+		httputils.RespondWithJsonError(w, "not authorized to update this resource", 403)
+		return
+	}
+
+	err = app.BlogService.UpdateBlogPost(r.Context(), &revisedPost, &storedPost)
+	if err != nil {
+		// TODO: typed errors for better client responses
+		app.Logger.Error("failed to persist blog post updates", "error", err, "location", "UpdateBlogPostHandler")
+		httputils.RespondWithJsonError(w, "internal service error", 500)
+		return
+	}
+
+	type Response struct {
+		Post model.BlogPost `json:"post"`
+	}
+
+	httputils.RespondWithJson(w, Response{
+		Post: storedPost,
+	})
 }
 
 func (app *App) DeleteBlogPostHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("not implemented"))
+	postID := r.PathValue("id")
+
+	storedPost, err := app.BlogService.FetchBlogPost(r.Context(), postID)
+	if err != nil {
+		app.Logger.Error("blog post for given ID does not exist", "error", err, "location", "DeleteBlogPostHandler")
+		httputils.RespondWithJsonError(w, fmt.Sprintf("invalid request: blog post with ID %s does not exist", postID), 400)
+		return
+	}
+
+	// validate user owns resource
+	userID, err := httputils.GetUserFromContext(r.Context())
+	if err != nil {
+		app.Logger.Error("failed to identify user", "error", err, "location", "DeleteBlogPostHandler")
+		httputils.RespondWithJsonError(w, "internal service error", 500)
+		return
+	}
+
+	if storedPost.AuthorID != userID {
+		app.Logger.Error("requestor does not own the blog post they are editing", "location", "DeleteBlogPostHandler", "originalAuthor", storedPost.AuthorID, "requestor", userID)
+		httputils.RespondWithJsonError(w, "not authorized to update this resource", 403)
+		return
+	}
+
+	app.BlogService.DeleteBlogPost(r.Context(), postID)
+
+	type Response struct {
+		PostID string `json:"post_id"`
+	}
+
+	httputils.RespondWithJson(w, Response{
+		PostID: postID,
+	})
 }
 
 func (app *App) AdminDeleteBlogPostHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("not implemented"))
+	postID := r.PathValue("id")
+
+	_, err := app.BlogService.FetchBlogPost(r.Context(), postID)
+	if err != nil {
+		app.Logger.Error("blog post for given ID does not exist", "error", err, "location", "AdminDeleteBlogPostHandler")
+		httputils.RespondWithJsonError(w, fmt.Sprintf("invalid request: blog post with ID %s does not exist", postID), 400)
+		return
+	}
+
+	app.BlogService.DeleteBlogPost(r.Context(), postID)
+
+	type Response struct {
+		PostID string `json:"post_id"`
+	}
+
+	httputils.RespondWithJson(w, Response{
+		PostID: postID,
+	})
 }
--- internal/db/posts.go
+++ internal/db/posts.go
@@ -5,7 +5,6 @@ import (
 	"errors"
 	"time"
 
-	"github.com/James-D-Wood/blog-api/internal/constant"
 	"github.com/James-D-Wood/blog-api/internal/model"
 )
 
@@ -18,9 +17,9 @@ var (
 type BlogService interface {
 	FetchBlogPost(ctx context.Context, id string) (model.BlogPost, error)
 	FetchPublishedBlogPosts(ctx context.Context) ([]model.BlogPost, error)
-	CreateBlogPost(ctx context.Context, blog *model.BlogPost) error
-	UpdateBlogPost(ctx context.Context, blog model.BlogPost) error
-	DeleteBlogPost(ctx context.Context, id string) (model.BlogPost, error)
+	CreateBlogPost(ctx context.Context, userID string, blog *model.BlogPost) error
+	UpdateBlogPost(ctx context.Context, newVersion *model.BlogPost, previousVersion *model.BlogPost) error
+	DeleteBlogPost(ctx context.Context, id string) error
 }
 
 // InMemoryBlogService implements BlogService using an in process data store
@@ -42,18 +41,18 @@ func (s *InMemoryBlogService) FetchBlogPost(ctx context.Context, id string) (mod
 func (s *InMemoryBlogService) FetchPublishedBlogPosts(ctx context.Context) ([]model.BlogPost, error) {
 	blogs := []model.BlogPost{}
 	for _, blog := range s.m {
-		// if blog is published
-		blogs = append(blogs, blog)
+		if blog.Status == model.PUBLISHED {
+			blogs = append(blogs, blog)
+		}
 	}
 	return blogs, nil
 }
 
-func (s *InMemoryBlogService) CreateBlogPost(ctx context.Context, post *model.BlogPost) error {
+func (s *InMemoryBlogService) CreateBlogPost(ctx context.Context, userID string, post *model.BlogPost) error {
 	// TODO: add required field validation - ie: title, description, contents
 
 	// set generated values
 	post.ID = assignUUID()
-	userID, _ := ctx.Value(constant.UserIDKey).(string)
 	post.AuthorID = userID
 	ts := time.Now().Format(time.RFC3339)
 	post.CreatedTS = ts
@@ -70,10 +69,32 @@ func (s *InMemoryBlogService) CreateBlogPost(ctx context.Context, post *model.Bl
 	return nil
 }
 
-func (s *InMemoryBlogService) UpdateBlogPost(ctx context.Context, blog model.BlogPost) error {
-	return errors.New("not implemented")
+func (s *InMemoryBlogService) UpdateBlogPost(ctx context.Context, newVersion *model.BlogPost, previousVersion *model.BlogPost) error {
+	// TODO: improve required field validations
+	if newVersion.Title == "" {
+		return errors.New("title cannot be empty")
+	}
+
+	ts := time.Now().Format(time.RFC3339)
+	previousVersion.UpdatedTS = ts
+
+	// only allow certain fields to be updated
+	previousVersion.Title = newVersion.Title
+	previousVersion.Summary = newVersion.Summary
+	previousVersion.Contents = newVersion.Contents
+	previousVersion.Status = newVersion.Status
+
+	// automatically add publication date
+	if newVersion.Status != previousVersion.Status && newVersion.Status == model.PUBLISHED {
+		previousVersion.PublishedTS = ts
+	}
+
+	s.m[previousVersion.ID] = *previousVersion
+
+	return nil
 }
 
-func (s *InMemoryBlogService) DeleteBlogPost(ctx context.Context, id string) (model.BlogPost, error) {
-	return model.BlogPost{}, errors.New("not implemented")
+func (s *InMemoryBlogService) DeleteBlogPost(ctx context.Context, id string) error {
+	delete(s.m, id)
+	return nil
 }
