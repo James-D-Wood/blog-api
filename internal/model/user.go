6da651abdfeb68519001d68e1477525c5447af79
James Wood <woodjames275@gmail.com>
2025-06-25 23:00:09 -0700

    fix: circular dependency

--- internal/model/user.go
+++ internal/model/user.go
@@ -1,26 +1,8 @@
 package model
 
-import (
-	"github.com/James-D-Wood/blog-api/internal/httputils"
-	"github.com/golang-jwt/jwt/v5"
-)
-
 type User struct {
 	ID       string
 	Username string
 	Name     string
 	IsAdmin  bool
-}
-
-// this is insecure - ideally we should be expiring our JWTs
-func (user *User) GenerateJWT() (string, error) {
-	token := jwt.NewWithClaims(
-		jwt.SigningMethodHS256,
-		jwt.MapClaims{
-			"user_id":  user.ID,
-			"is_admin": user.IsAdmin,
-		},
-	)
-
-	return token.SignedString(httputils.HMACSecret)
 }
